49a054f2f19a944d507ed8d9bfb1ccc7632ce735
CristiBalea <balea.cristian90@gmail.com>
2015-10-07 23:40:18 +0300

    small change

--- PythagoreanTheorem/src/main/PythagTheorem.java
+++ PythagoreanTheorem/src/main/PythagTheorem.java
@@ -2,10 +2,6 @@ package main;
 
 public class PythagTheorem {
 
-	static double sideA;
-	static double sideB;
-	static double hypotenuse;
-
 	public static void main(String[] args) {
 
 		System.out.println(getHypotenuse(3, 4));
